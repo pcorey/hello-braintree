a641906d98fa559c5f2827cf1523f659b3f7e63f
Pete Corey <petecorey@gmail.com>
2015-04-17 17:58:34 -0700

    First commit

---
+++ packages/braintree/package.js
@@ -0,0 +1,21 @@
+Package.describe({
+  name: 'braintree',
+  version: '0.0.1',
+  // Brief, one-line summary of the package.
+  summary: '',
+  // URL to the Git repository containing the source code for this package.
+  git: '',
+  // By default, Meteor will default to using README.md for documentation.
+  // To avoid submitting documentation, set this field to null.
+  documentation: 'README.md'
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.1.0.2');
+  api.addFiles('braintree.js');
+  api.export('Braintree', ['server']);
+});
+
+Npm.depends({
+    braintree: '1.23.0'
+});
